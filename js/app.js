8c623bb37664cec009c7daf32aa29b8b87724c8e
Knar <zsimpso1@binghamton.edu>
2018-09-22 16:45:07 -0400

    added ingredient item components with on-click delete

--- js/app.js
+++ js/app.js
@@ -1,18 +1,36 @@
-const app = new Vue({
+Vue.component('ingredient-item', {
+    props: ['ingredient'],
+    template: `
+        <div class="ingredient-item" v-on:click="$emit('delete-ingredient', ingredient.id)">
+            {{ ingredient.label }}
+        </div>
+    `,
+})
+
+new Vue({
     el: '#app',
     data() {
         return {
-            ingredientsList: []
+            ingredientsList: [],
+            id_gen: 0,
         }
     },
     methods: {
         addIngredient: function() {
-            const input = document.getElementById('inputForm')
+            const input = document.getElementById('ingredient-input')
 
             if (input.value !== '') {
-                this.ingredientsList.push(input.value)
+                this.id_gen++
+                this.ingredientsList.push( { id: this.id_gen, label: input.value } )
                 input.value = ""
             }
+        },
+        deleteIngredient: function(id) {
+            console.log('delete ingredient got called')
+            let index = this.ingredientsList.findIndex( function(item) {
+                return item.id === id
+            })
+            if (index !== -1) this.ingredientsList.splice(index, 1)
         }
     }
 })
