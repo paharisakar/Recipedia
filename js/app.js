d9e6af7ff5f217926dfde32cac6037fb4a7dcd12
Knar <zsimpso1@binghamton.edu>
2018-09-26 20:28:50 -0400

    added links to recipe components, and uncommented the viewport meta tag thing that Parth added earlier to render on mobile better

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ Vue.component('recipes-list-item', {
     props: ['recipe'],
     template: `
         <div class="recipes-list-item">
-            <h2> {{ recipe.title }} </h2>
+            <a v-bind:href="'' + recipe.url" target="_blank"><h2> {{ recipe.title }} </h2></a>
             {{ recipe.body }}
         </div>
     `
@@ -23,7 +23,27 @@ new Vue({
         return {
             ingr_id_gen: 0,
             ingredients: [],
-            recipes: [],
+            recipes: [
+                {
+                    id: 1,
+                    title: "Chana Masala",
+                    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...",
+                    url: "https://minimalistbaker.com/easy-chana-masala/",
+                },
+                {
+                    id: 2,
+                    title: "Chicken Noodle Soup",
+                    body: "Diam vulputate ut pharetra sit amet aliquam id diam. Tempor commodo ullamcorper a lacus. Fermentum posuere urna nec tincidunt. Massa id neque aliquam vestibulum. Elementum facilisis leo vel fringilla est...",
+                    url: "https://www.tasteofhome.com/recipes/the-ultimate-chicken-noodle-soup/",
+                },
+                {
+                    id: 3,
+                    title: "Authentic Pho",
+                    body: "Facilisis mauris sit amet massa. Eget nunc scelerisque viverra mauris in. Malesuada nunc vel risus commodo viverra. Volutpat blandit aliquam etiam erat velit scelerisque in. Cras pulvinar mattis nunc...",
+                    url: "https://www.allrecipes.com/recipe/228443/authentic-pho/",
+                }
+            ],
+            show_recipes: false
         }
     },
     methods: {
@@ -34,38 +54,21 @@ new Vue({
                 this.ingr_id_gen++
                 this.ingredients.push( { id: this.ingr_id_gen, label: input.value } )
                 input.value = ""
+                if (!this.show_recipes) {
+                    this.show_recipes = true
+                }
             }
-            this.updateRecipes()
+
         },
         deleteIngredient: function(id) {
             let index = this.ingredients.findIndex( function(item) {
                 return item.id == id
             })
-            if (index != -1) this.ingredients.splice(index, 1)
-            this.updateRecipes()
-        },
-        updateRecipes: function() {
-            if (this.ingredients.length == 0) {
-                this.recipes = []
-            }
-            else if (this.recipes.length == 0) {
-                this.recipes.push( {
-                    id: 1,
-                    title: "Chana Masala",
-                    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...",
-                })
-
-                this.recipes.push( {
-                    id: 2,
-                    title: "Chicken Noodle Soup",
-                    body: "Diam vulputate ut pharetra sit amet aliquam id diam. Tempor commodo ullamcorper a lacus. Fermentum posuere urna nec tincidunt. Massa id neque aliquam vestibulum. Elementum facilisis leo vel fringilla est...",
-                })
-
-                this.recipes.push( {
-                    id: 3,
-                    title: "Authentic Pho",
-                    body: "Facilisis mauris sit amet massa. Eget nunc scelerisque viverra mauris in. Malesuada nunc vel risus commodo viverra. Volutpat blandit aliquam etiam erat velit scelerisque in. Cras pulvinar mattis nunc...",
-                })
+            if (index != -1) {
+                this.ingredients.splice(index, 1)
+                if (this.recipes.length == 0) {
+                    this.show_recipes = false
+                }
             }
         }
     }
