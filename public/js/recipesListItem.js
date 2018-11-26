3a481adcd305b0d6a802df33520e3c749dbbf3cc
knar <zakjsimpson@gmail.com>
2018-11-25 19:34:43 -0500

    Open links in new tab

--- public/js/recipesListItem.js
+++ public/js/recipesListItem.js
@@ -2,7 +2,7 @@ Vue.component('recipes-list-item', {
     props: ['recipe'],
     template: `
         <div class="recipes-list-item" v-on:click="$emit('click-recipe', recipe.id)" title="Click to expand/shrink this recipe.">
-            <a v-bind:href="recipe.url">
+            <a v-bind:href="recipe.url" target="_blank">
                 <img v-bind:src="recipe.img" />
                 <h3>{{ recipe.title }}</h3>
             </a>
