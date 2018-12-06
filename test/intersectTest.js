f74d52c541e7bbc771ef836d2459651f069b00cf
Songtao Wei <songtaow@buffalo.edu>
2018-12-05 21:44:30 -0500

    Add unit tests for database query and intersect function

--- test/intersectTest.js
+++ test/intersectTest.js
@@ -10,4 +10,20 @@ describe('Intersection of Lists',function(){
     let result = server.intersectOfLists([['milk'],['milk','egg']]);
     expect(result).to.eql(['milk'])
   })
+  it("intersection of lists [] and ['milk', 'egg'] should return []",function(){
+    let result = server.intersectOfLists([[],['milk','egg']]);
+    expect(result).to.eql([])
+  })
+  it("intersection of lists ['chicken'] and ['milk', 'egg'] should return []",function(){
+    let result = server.intersectOfLists([['chicken'],['milk','egg']]);
+    expect(result).to.eql([])
+  })
+  it("intersection of lists ['milk', 'egg'] and ['milk', 'egg'] should return ['milk', 'egg']",function(){
+    let result = server.intersectOfLists([['milk', 'egg'],['milk','egg']]);
+    expect(result).to.eql(['milk', 'egg'])
+  })
+  it("intersection of lists [] and [] should return []",function(){
+    let result = server.intersectOfLists([[],[]]);
+    expect(result).to.eql([])
+  })
 })
--- test/queryTest.js
+++ test/queryTest.js
@@ -7,16 +7,41 @@ let server = new Server(http)
 
 describe('Mapping Query', () => {
     it(`mapping query for 'honey' should return 'select recipe from ingredientMapping where ingredient = "honey"'`, () => {
-        assert.equal(server.mappingQuery('honey'), `select recipe from ingredientMapping where ingredient = "honey"`)
+        assert.equal(server.queryRecipesFromIngredient('honey'), `select recipe from ingredientMapping where ingredient = "honey"`)
     })
-
     it(`mapping query for 'soy sauce' should return 'select recipe from ingredientMapping where ingredient = "soy sauce"'`, () => {
-        assert.equal(server.mappingQuery('soy sauce'), `select recipe from ingredientMapping where ingredient = "soy sauce"`)
+        assert.equal(server.queryRecipesFromIngredient('soy sauce'), `select recipe from ingredientMapping where ingredient = "soy sauce"`)
+    })
+    it(`mapping query for 'chicken' should return 'select recipe from ingredientMapping where ingredient = "chicken"'`, () => {
+        assert.equal(server.queryRecipesFromIngredient('chicken'), `select recipe from ingredientMapping where ingredient = "chicken"`)
     })
 })
 
-describe('Instructions Query', () => {
-    it(`instructions query for 'chicken alfredo' should return 'select instructions from instruction where Dish = "chicken alfredo"'`, () => {
-        assert.equal(server.instructionsQuery('chicken alfredo'), `select instructions from instruction where Dish = "chicken alfredo"`)
+describe('Recipe details selecting Query', () => {
+    it(`Recipe details query for recipe 'Apple Puff Pancake' should return 'select * from recipeUrl where recipeName = "Apple Puff Pancake"`, () => {
+        assert.equal(server.queryDetailsFromRecipe('Apple Puff Pancake'), `select * from recipeUrl where recipeName = "Apple Puff Pancake"`)
+    })
+    it(`Recipe details query for recipe 'Breakfast Pies' should return 'select * from recipeUrl where recipeName = "Breakfast Pies"`, () => {
+        assert.equal(server.queryDetailsFromRecipe('Breakfast Pies'), `select * from recipeUrl where recipeName = "Breakfast Pies"`)
+    })
+    it(`Recipe details query for recipe 'Alyson\'s Broccoli Salad' should return 'select * from recipeUrl where recipeName = "Alyson\'s Broccoli Salad"`, () => {
+        assert.equal(server.queryDetailsFromRecipe('Alyson\'s Broccoli Salad'), `select * from recipeUrl where recipeName = "Alyson\'s Broccoli Salad"`)
+    })
+})
+
+describe('All Ingredients selecting Query', () => {
+    it(`query for all ingredients should return 'select ingredient from ingredientMapping'`, () => {
+        assert.equal(server.queryAllIngredients(), `select ingredient from ingredientMapping`)
+    })
+})
+describe('Recipe phrase searching Query ', () => {
+    it(`phrase searching query for input word 'soup' should return 'select * from recipeUrl where recipeName like '%soup%'`, () => {
+        assert.equal(server.queryRecipesLikePhrase('soup'), `select * from recipeUrl where recipeName like '%soup%'`)
+    })
+    it(`phrase searching query for input word 'pizza' should return 'select * from recipeUrl where recipeName like '%pizza%'`, () => {
+        assert.equal(server.queryRecipesLikePhrase('pizza'), `select * from recipeUrl where recipeName like '%pizza%'`)
+    })
+    it(`phrase searching query for input word 'salad' should return 'select * from recipeUrl where recipeName like '%salad%'`, () => {
+        assert.equal(server.queryRecipesLikePhrase('salad'), `select * from recipeUrl where recipeName like '%salad%'`)
     })
 })
